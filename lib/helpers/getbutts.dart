ef286ce720359100db497f95397d5553b6736846
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-01-25 17:51:38 +0530

    refactor: used objects

--- lib/helpers/getbutts.dart
+++ lib/helpers/getbutts.dart
@@ -1,42 +1,49 @@
 import 'package:flutter/material.dart';
 
+class Butt {
+  MaterialColor color;
+  String buttText;
+  String messageText;
+  Butt(this.color, this.buttText, this.messageText);
+}
+
 /// Returns 4 elevated butts in a list in various colors!
 ///
-List<ElevatedButton> getButts() {
-  // As we just need 4 buttons, with only the background color changing,
-  // we can use a list with the data that varies (in this case the bgColor)
-  // Here I have also type constrained the variable as a good practice
-  List<MaterialColor> buttColors = <MaterialColor>[
-    Colors.red,
-    Colors.yellow,
-    Colors.blue,
-    Colors.green,
+List<ElevatedButton> getButts(BuildContext context) {
+  const commonName = 'Press Me Daddy';
+  List<Butt> buttColors = <Butt>[
+    Butt(Colors.red, commonName, 'Joe'),
+    Butt(Colors.yellow, commonName, 'Ligma'),
+    Butt(Colors.blue, commonName, 'Sugon'),
+    Butt(Colors.green, commonName, 'Sugma'),
   ];
 
   // I used Elevated Button cuz it has a nice look to it...
   // compared to the others :P
-
   var butts = <ElevatedButton>[];
 
   // Now we will iterate through the color list to generate the unique buttons
   // which will be visible in the app
-
   buttColors.asMap().forEach((key, value) {
     butts.add(
       ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          final text = value.messageText;
+          final snackBar = SnackBar(content: Text(text));
+          ScaffoldMessenger.of(context)
+            ..removeCurrentSnackBar()
+            ..showSnackBar(snackBar);
+        },
         style: ElevatedButton.styleFrom(
-          backgroundColor: value,
+          backgroundColor: value.color,
           padding: const EdgeInsets.symmetric(
               horizontal: 10, vertical: 5), // aesthetics 🤌
         ),
-        child:
-            Text("Press Me! I am Button ${key + 1}"), // unique identification
+        child: Text(
+            "${value.buttText}, I am Button ${key + 1}"), // unique identification
       ),
     );
   });
-
   // Now we will return the buttons list!
   return butts;
 }
-// 42 line perfection 🤌
--- lib/main.dart
+++ lib/main.dart
@@ -22,28 +22,35 @@ class LGDemoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: const Text('LG Demo App'),
-        ),
-        body: Center(
-          child: Column(
-              // Arranging the buttons in a column
-              mainAxisAlignment: MainAxisAlignment.center, // For aesthetics
-              children: [
-                // Loading Logo Image from the LG site!
-                FadeInImage.assetNetwork(
-                  placeholder: 'assets/images/loadingloader.gif',
-                  image:
-                      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjzI4JzY6oUy-dQaiW-HLmn5NQ7qiw7NUOoK-2cDU9cI6JwhPrNv0EkCacuKWFViEgXYrCFzlbCtHZQffY6a73j6_ATFjfeU7r6OxXxN5K8sGjfOlp3vvd6eCXZrozlu34fUG5_cKHmzZWa4axb-vJRKjLr2tryz0Zw30gTv3S0ET57xsCiD25WMPn3wA/s800/LIQUIDGALAXYLOGO.png',
-                ),
-                // Calling a modular function to generate dummy buttons
-                // and spreading it
-                ...getButts(),
-              ]),
-        ),
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: const Text('LG Demo App'),
+      ),
+      body: Center(
+        child: Column(
+            // Arranging the buttons in a column
+            mainAxisAlignment: MainAxisAlignment.center, // For aesthetics
+            children: [
+              // Loading Logo Image from the LG site!
+              FadeInImage.assetNetwork(
+                placeholder: 'assets/images/loadingloader.gif',
+                image:
+                    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjzI4JzY6oUy-dQaiW-HLmn5NQ7qiw7NUOoK-2cDU9cI6JwhPrNv0EkCacuKWFViEgXYrCFzlbCtHZQffY6a73j6_ATFjfeU7r6OxXxN5K8sGjfOlp3vvd6eCXZrozlu34fUG5_cKHmzZWa4axb-vJRKjLr2tryz0Zw30gTv3S0ET57xsCiD25WMPn3wA/s800/LIQUIDGALAXYLOGO.png',
+              ),
+              // Calling a modular function to generate dummy buttons
+              // and spreading it
+              ...getButts(context),
+            ]),
       ),
     );
   }
