aeacf3146abded17afcd5d8b08b94ff34eaaceb5
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-01-25 14:05:04 +0530

    feat: buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,52 @@ class LGDemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
-        title: const Text('LGDemoApp'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: const Text('LG Demo App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 5)),
+                child: const Text('First!'),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 5)),
+                child: const Text('Second!'),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 5)),
+                child: const Text('Third!'),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 5)),
+                child: const Text('Fourth!'),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(
-        child: const Text('Hello World 😀'),
-      ),
-    ));
+    );
   }
 }
